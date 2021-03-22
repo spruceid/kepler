faf2e134c11007cf4a49a55fb67eae1cf9eecf50
Charles Cunningham <c.a.cunningham6@gmail.com>
2021-03-22 16:54:46 +0000

    use multihash for route and stub basic routes

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,46 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use]
+extern crate rocket;
+extern crate multihash;
+
+use rocket::{
+    data::{Capped, Data, TempFile, ToByteUnit},
+    response::NamedFile,
+};
+
+// 10 megabytes
+const STREAM_LIMIT: usize = 10.megabytes();
+
+struct MH(multihash::Multihash);
+
+impl MH {
+    pub fn read<R: std::io::Read>(r: R) -> Result<Self, multihash::Error> {
+        Ok(Self(multihash::Multihash::read(r)?))
+    }
+}
+
+// Orphan rule requires a wrapper type for this :(
+impl<'a> rocket::request::FromParam<'_> for MH {
+    type Error = multihash::Error;
+    fn from_param(param: &'a str) -> Result<Self, multihash::Error> {
+        Self::read(param.bytes())
+    }
+}
+
+#[get("/<hash>")]
+fn get_content(hash: MH) -> Option<NamedFile> {
+    todo!()
+}
+
+#[post("/", format = "plain", data = "<data>")]
+fn put_content(data: Data) -> String {
+    todo!()
+}
+
 fn main() {
-    println!("Hello, kepler!");
+    rocket::ignite()
+        .mount("/", routes![get_content])
+        .mount("/", routes![put_content])
+        .launch();
 }
