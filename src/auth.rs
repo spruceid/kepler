0fdb7e2d8c1131d574f77eec0dd3bc89370530ae
Charles Cunningham <c.a.cunningham6@gmail.com>
2021-03-26 13:02:31 +0000

    try some Auth traits

--- src/auth.rs
+++ src/auth.rs
@@ -4,29 +4,49 @@ use rocket::{
     request::{FromRequest, Outcome, Request},
 };
 
-pub trait AuthrorizationStrategy: Sized {
-    fn authorize<'a, T: Iterator<Item = &'a str>>(auth_data: T) -> Result<Self>;
+pub trait AuthorizationToken: Sized {
+    fn extract<'a, T: Iterator<Item = &'a str>>(auth_data: T) -> Result<Self>;
+    fn header_key() -> &'static str;
 }
 
-pub struct Authorization<S: AuthrorizationStrategy>(S);
+pub trait AuthrorizationStrategy: Sized {
+    type Token: AuthorizationToken;
+    type Action;
+    fn authorize(&self, auth_token: Self::Token) -> Result<Self::Action>;
+}
 
 #[derive(PartialEq)]
-pub struct DummyAuth;
+pub struct DummyToken;
 
-impl AuthrorizationStrategy for DummyAuth {
-    fn authorize<'a, T: Iterator<Item = &'a str>>(auth_data: T) -> Result<Self> {
+pub struct DummyAuthorization;
+
+pub enum AuthToken {
+    None,
+    TezosSignature(String),
+}
+
+impl AuthorizationToken for DummyToken {
+    fn extract<'a, T: Iterator<Item = &'a str>>(auth_data: T) -> Result<Self> {
         Ok(Self)
+    }
+    fn header_key() -> &'static str {
+        ""
     }
 }
 
-impl<'a, 'r, T> FromRequest<'a, 'r> for Authorization<T>
-where
-    T: AuthrorizationStrategy,
-{
+impl AuthrorizationStrategy for DummyAuthorization {
+    type Token = DummyToken;
+    type Action = ();
+    fn authorize(&self, auth_token: Self::Token) -> Result<Self::Action> {
+        Ok(())
+    }
+}
+
+impl<'a, 'r, T> FromRequest<'a, 'r> for AuthToken {
     type Error = anyhow::Error;
 
     fn from_request(request: &'a Request<'r>) -> Outcome<Self, Self::Error> {
-        match T::authorize(request.headers().get("Authorization")) {
+        match T::extract(request.headers().get(T::header_key())) {
             Ok(auth) => Outcome::Success(Self(auth)),
             Err(e) => Outcome::Failure((Status::Unauthorized, anyhow!(format!("{}", e)))),
         }
