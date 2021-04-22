20811475daef7de07b1dad779a23e56f910e949e
Charles Cunningham <c.a.cunningham6@gmail.com>
2021-04-22 21:01:09 +0200

    pin stored content, unpin on delete

--- src/ipfs.rs
+++ src/ipfs.rs
@@ -33,10 +33,12 @@ impl ContentAddressedStorage for Ipfs<DefaultParams> {
     async fn get(&self, address: &Cid) -> Result<Option<Vec<u8>>, Self::Error> {
         // TODO this api returns Result<Block, anyhow::Error>, with an err thrown for no block found
         // until this API changes (a breaking change), we will error here when no block found
+        self.alias(address.to_bytes(), Some(address))?;
         Ok(Some(self.get(address)?.data().to_vec()))
     }
     async fn delete(&self, address: &Cid) -> Result<(), Self::Error> {
         // TODO this does not enforce deletion across the network, we need to devise a method for that via the pubsub stuff
+        self.alias(address.to_bytes(), None)?;
         Ok(self.remove_record(&address.hash().to_bytes().into()))
     }
 }
