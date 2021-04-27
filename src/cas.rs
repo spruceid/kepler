49050b5d24a251092cbd58075f2d7b6a174bf7c3
Charles Cunningham <c.a.cunningham6@gmail.com>
2021-04-28 01:55:20 +0200

    read in form guard and add simple test

--- src/cas.rs
+++ src/cas.rs
@@ -5,11 +5,7 @@ use rocket::tokio::io::AsyncRead;
 #[rocket::async_trait]
 pub trait ContentAddressedStorage: Send + Sync {
     type Error;
-    async fn put<C: AsyncRead + Send + Unpin>(
-        &self,
-        content: &mut C,
-        codec: SupportedCodecs,
-    ) -> Result<Cid, Self::Error>;
+    async fn put(&self, content: &[u8], codec: SupportedCodecs) -> Result<Cid, Self::Error>;
     async fn get(&self, address: &Cid) -> Result<Option<Vec<u8>>, Self::Error>;
     async fn delete(&self, address: &Cid) -> Result<(), Self::Error>;
 }
