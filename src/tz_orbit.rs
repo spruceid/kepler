3c2436f0bfc65e5270ac30b7a7845b6c6fff2621
Charles Cunningham <c.a.cunningham6@gmail.com>
2021-07-01 18:19:54 +0200

    fetch state using tzkt

--- src/tz_orbit.rs
+++ src/tz_orbit.rs
@@ -1,5 +1,6 @@
 use anyhow::Result;
 use reqwest;
+use serde::Deserialize;
 use serde_json::Value;
 use std::collections::HashMap as Map;
 
@@ -9,17 +10,55 @@ pub struct Manifest {
     hosts: Map<String, Vec<String>>,
 }
 
-const DEFAULT_API: &str = "http://localhost:14000";
+#[derive(Deserialize)]
+pub struct Storage {
+    admins: u64,
+    hosts: u64,
+}
+
+#[derive(Debug, Deserialize)]
+struct BigmapKey<V> {
+    active: bool,
+    key: String,
+    value: V,
+}
+
+#[derive(Debug, Deserialize)]
+struct EmptyObject {}
+
+const DEFAULT_TZKT_API: &str = "http://localhost:5000";
 
 async fn get_orbit_state(address: &str) -> Result<Manifest> {
-    let url = format!("{}/v1/contract/sandboxnet/{}/storage", DEFAULT_API, address);
-    println!("{}", &url);
-    let resp = reqwest::get(&url)
+    let storage_url = format!("{}/v1/contracts/{}/storage", DEFAULT_TZKT_API, address);
+    let storage = reqwest::get(&storage_url).await?.json::<Storage>().await?;
+
+    Ok(Manifest {
+        admins: reqwest::get(format!(
+            "{}/v1/bigmaps/{}/keys",
+            DEFAULT_TZKT_API, storage.admins
+        ))
         .await?
-        .json::<Map<String, Value>>()
-        .await?;
-    println!("{:#?}", resp);
-    Ok(Default::default())
+        .json::<Vec<BigmapKey<EmptyObject>>>()
+        .await?
+        .into_iter()
+        .filter_map(|k| if k.active { Some(k.key) } else { None })
+        .collect(),
+        hosts: reqwest::get(format!(
+            "{}/v1/bigmaps/{}/keys",
+            DEFAULT_TZKT_API, storage.hosts
+        ))
+        .await?
+        .json::<Vec<BigmapKey<Vec<String>>>>()
+        .await?
+        .into_iter()
+        .filter_map(|k| if k.active { Some(k) } else { None })
+        .fold(Map::new(), |mut acc, k| {
+            if k.active {
+                acc.insert(k.key, k.value);
+            };
+            acc
+        }),
+    })
 }
 
 #[cfg(test)]
@@ -28,8 +67,9 @@ mod tests {
 
     #[test]
     async fn test() -> Result<()> {
-        let m = get_orbit_state("KT1EEAkHJEAJhqhQChcMaEuG2t3KdenbsVRh").await?;
-        // println!("{:#?}", m);
+        let address = "KT1L7hDDhXynuMoyRGoQNBNUHZEM5iBRu24U";
+        let m = get_orbit_state(address).await?;
+        println!("{:#?}", m);
         assert!(false);
         Ok(())
     }
