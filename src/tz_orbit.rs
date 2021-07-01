7712f3a9e17dfe82c7300624b1b5b00b4cf0e079
Charles Cunningham <c.a.cunningham6@gmail.com>
2021-07-01 19:23:57 +0200

    cleanup and type conveniences

--- src/tz_orbit.rs
+++ src/tz_orbit.rs
@@ -1,13 +1,13 @@
 use anyhow::Result;
+use ipfs_embed::{Multiaddr, PeerId};
 use reqwest;
 use serde::Deserialize;
-use serde_json::Value;
-use std::collections::HashMap as Map;
+use std::{collections::HashMap as Map, convert::TryFrom, str::FromStr};
 
 #[derive(Default, Debug)]
 pub struct Manifest {
     admins: Vec<String>,
-    hosts: Map<String, Vec<String>>,
+    hosts: Map<PeerId, Vec<Multiaddr>>,
 }
 
 #[derive(Deserialize)]
@@ -17,47 +17,50 @@ pub struct Storage {
 }
 
 #[derive(Debug, Deserialize)]
-struct BigmapKey<V> {
+struct BigmapKey<K = String, V = EmptyObject> {
     active: bool,
-    key: String,
+    key: K,
     value: V,
 }
 
 #[derive(Debug, Deserialize)]
 struct EmptyObject {}
 
+#[derive(Deserialize)]
+#[serde(try_from = "&str")]
+struct PID(pub PeerId);
+
+impl TryFrom<&str> for PID {
+    type Error = <PeerId as FromStr>::Err;
+    fn try_from(v: &str) -> Result<Self, Self::Error> {
+        Ok(Self(PeerId::from_str(v)?))
+    }
+}
+
 const DEFAULT_TZKT_API: &str = "http://localhost:5000";
 
-async fn get_orbit_state(address: &str) -> Result<Manifest> {
-    let storage_url = format!("{}/v1/contracts/{}/storage", DEFAULT_TZKT_API, address);
+async fn get_orbit_state(tzkt_api: &str, address: &str) -> Result<Manifest> {
+    let storage_url = format!("{}/v1/contracts/{}/storage", tzkt_api, address);
     let storage = reqwest::get(&storage_url).await?.json::<Storage>().await?;
 
     Ok(Manifest {
-        admins: reqwest::get(format!(
-            "{}/v1/bigmaps/{}/keys",
-            DEFAULT_TZKT_API, storage.admins
-        ))
-        .await?
-        .json::<Vec<BigmapKey<EmptyObject>>>()
-        .await?
-        .into_iter()
-        .filter_map(|k| if k.active { Some(k.key) } else { None })
-        .collect(),
-        hosts: reqwest::get(format!(
-            "{}/v1/bigmaps/{}/keys",
-            DEFAULT_TZKT_API, storage.hosts
-        ))
-        .await?
-        .json::<Vec<BigmapKey<Vec<String>>>>()
-        .await?
-        .into_iter()
-        .filter_map(|k| if k.active { Some(k) } else { None })
-        .fold(Map::new(), |mut acc, k| {
-            if k.active {
-                acc.insert(k.key, k.value);
-            };
-            acc
-        }),
+        admins: reqwest::get(format!("{}/v1/bigmaps/{}/keys", tzkt_api, storage.admins))
+            .await?
+            .json::<Vec<BigmapKey>>()
+            .await?
+            .into_iter()
+            .filter_map(|k| if k.active { Some(k.key) } else { None })
+            .collect(),
+        hosts: reqwest::get(format!("{}/v1/bigmaps/{}/keys", tzkt_api, storage.hosts))
+            .await?
+            .json::<Vec<BigmapKey<PID, Vec<Multiaddr>>>>()
+            .await?
+            .into_iter()
+            .filter_map(|k| if k.active { Some(k) } else { None })
+            .fold(Map::new(), |mut acc, k| {
+                acc.insert(k.key.0, k.value);
+                acc
+            }),
     })
 }
 
@@ -68,7 +71,7 @@ mod tests {
     #[test]
     async fn test() -> Result<()> {
         let address = "KT1L7hDDhXynuMoyRGoQNBNUHZEM5iBRu24U";
-        let m = get_orbit_state(address).await?;
+        let m = get_orbit_state(DEFAULT_TZKT_API, address).await?;
         println!("{:#?}", m);
         assert!(false);
         Ok(())
