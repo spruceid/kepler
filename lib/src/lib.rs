1e877096d447c9e17fca716e44a4bbe3848e653c
Jacob <jacobbward0@gmail.com>
2022-05-19 14:00:00 +0100

    Investigate failing invocations.

--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -3,21 +3,3 @@ pub mod zcap;
 
 pub use didkit;
 pub use ssi;
-
-#[cfg(feature = "cacao_to_zcap")]
-pub mod cacao_zcap {
-    pub use ::cacao_zcap::translation::cacao_to_zcap::{cacao_to_zcap, CacaoToZcapError};
-}
-
-#[cfg(feature = "cacao_to_zcap")]
-pub mod cacaos {
-    pub use ::cacao_zcap::cacaos::{
-        siwe_cacao::{SIWESignature, SignInWithEthereum},
-        BasicSignature, CACAO,
-    };
-}
-
-#[cfg(feature = "cacao_to_zcap")]
-pub mod siwe {
-    pub use ::cacao_zcap::cacaos::siwe::{nonce::generate_nonce, Message, TimeStamp, Version};
-}
