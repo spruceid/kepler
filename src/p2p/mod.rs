ff46d56c4c56173499cc02da2ef43bf29428e9d9
chunningham <c.a.cunningham6@gmail.com>
2022-11-10 10:38:56 +0100

    minor builder refactors

--- src/p2p/mod.rs
+++ src/p2p/mod.rs
@@ -1,36 +1,49 @@
-use core::time::Duration;
-use derive_builder::Builder;
 use libp2p::{identify::Config as OIdentifyConfig, identity::PublicKey};
 pub mod behaviour;
 pub mod relay;
-
-#[derive(Builder, Default, Debug, Clone)]
-pub struct IdentifyConfig {
-    #[builder(setter(into), default = "Duration::from_millis(500)")]
-    initial_delay: Duration,
-    #[builder(setter(into), default = "Duration::from_secs(300)")]
-    interval: Duration,
-    #[builder(setter(into), default = "false")]
-    push_listen_addr_updates: bool,
-    #[builder(setter(into), default = "0")]
-    cache_size: usize,
-}
+pub mod transport;
 
 const PROTOCOL_VERSION: &'static str = "kepler/0.1.0";
 
+pub use builder::IdentifyConfig;
+
+mod builder {
+    use core::time::Duration;
+    use derive_builder::Builder;
+    use libp2p::{identify::Config as OIdentifyConfig, identity::PublicKey};
+    #[derive(Builder, Default, Debug, Clone)]
+    #[builder(build_fn(skip), setter(into), name = "IdentifyConfig", derive(Debug))]
+    pub struct IdentifyConfigDummy {
+        #[builder(field(type = "String"), default = "PROTOCOL_VERSION.into()")]
+        protocol_version: String,
+        #[builder(field(type = "Duration"), default = "Duration::from_millis(500)")]
+        initial_delay: Duration,
+        #[builder(field(type = "Duration"), default = "Duration::from_secs(300)")]
+        interval: Duration,
+        #[builder(field(type = "bool"), default = "false")]
+        push_listen_addr_updates: bool,
+        #[builder(field(type = "usize"), default = "0")]
+        cache_size: usize,
+    }
+    pub fn convert(config: IdentifyConfig, key: PublicKey) -> OIdentifyConfig {
+        OIdentifyConfig::new(config.protocol_version, key)
+            .with_initial_delay(config.initial_delay)
+            .with_interval(config.interval)
+            .with_push_listen_addr_updates(config.push_listen_addr_updates)
+            .with_cache_size(config.cache_size)
+    }
+}
+
 impl IdentifyConfig {
     fn to_config(self, key: PublicKey) -> OIdentifyConfig {
-        OIdentifyConfig::new(PROTOCOL_VERSION.to_string(), key)
-            .with_initial_delay(self.initial_delay)
-            .with_interval(self.interval)
-            .with_push_listen_addr_updates(self.push_listen_addr_updates)
-            .with_cache_size(self.cache_size)
+        builder::convert(self, key)
     }
 }
 
 impl From<OIdentifyConfig> for IdentifyConfig {
     fn from(c: OIdentifyConfig) -> Self {
         Self {
+            protocol_version: c.protocol_version,
             initial_delay: c.initial_delay,
             interval: c.interval,
             push_listen_addr_updates: c.push_listen_addr_updates,
