d76afd93037c3257975f392f24cea84bbea966c5
chunningham <c.a.cunningham6@gmail.com>
2023-04-05 13:11:45 +1000

    simplify LimitedReader impl with just 1 u64 field

--- src/routes/util.rs
+++ src/routes/util.rs
@@ -13,25 +13,19 @@ use std::{
 pub struct LimitedReader<R> {
     #[pin]
     inner: R,
-    limit: u64,
-    written: u64,
+    remaining: u64,
 }
 
 impl<R> LimitedReader<R> {
     pub fn new(inner: R, limit: u64) -> Self {
         Self {
             inner,
-            limit,
-            written: 0,
+            remaining: limit,
         }
     }
 
-    pub fn limit(&self) -> u64 {
-        self.limit
-    }
-
-    pub fn written(&self) -> u64 {
-        self.written
+    pub fn remaining_limit(&self) -> u64 {
+        self.remaining
     }
 }
 
@@ -49,21 +43,15 @@ where
         buf: &mut [u8],
     ) -> Poll<Result<usize, IoError>> {
         let this = self.project();
-        // calculate the number of bytes that can be read
-        let max_remaining = if let Some(remaining) = (*this.limit).checked_sub(*this.written) {
-            // it's ok if remaining is 0 here, as writing 0 bytes won't change anything
-            remaining
-        } else {
-            // limit already exceeded somehow
-            return Poll::Ready(Err(IoError::new(ErrorKind::Other, LimitExceeded)));
-        };
 
         match this.inner.poll_read(cx, buf) {
-            Poll::Ready(Ok(n)) if n as u64 > max_remaining => {
+            Poll::Ready(Ok(n)) if n as u64 > *this.remaining => {
                 Poll::Ready(Err(IoError::new(ErrorKind::Other, LimitExceeded)))
             }
             Poll::Ready(Ok(n)) => {
-                *this.written += n as u64;
+                // it's ok if remaining is 0 here, as writing 0 bytes won't change anything
+                // also we checked n > remaining above so it shouldnt underflow
+                *this.remaining -= n as u64;
                 Poll::Ready(Ok(n))
             }
             r => r,
