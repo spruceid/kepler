0856133deee1906c32b12e225ea07f1213139b83
chunningham <c.a.cunningham6@gmail.com>
2023-06-21 16:34:46 +0200

    remove unused files

--- kepler-core/src/lib.rs
+++ kepler-core/src/lib.rs
@@ -1,18 +1,15 @@
-mod db;
+pub mod db;
 pub mod events;
 pub mod hash;
-pub mod manager;
 pub mod manifest;
 pub mod migrations;
 pub mod models;
-pub mod orbit;
 pub mod relationships;
 pub mod storage;
 pub mod types;
 mod util;
 
 pub use db::Commit;
-pub use manager::{InitError, OrbitPeerManager};
-pub use orbit::OrbitPeer;
+pub use db::OrbitDatabase;
 pub use sea_orm;
 pub use sea_orm_migration;
